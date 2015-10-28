a7fe15d2053fa133bef55bcbf112cacdcb5c5f24
pulka <a@b.c>
2015-10-28 15:45:00 +0100

    stuff

--- player.js
+++ player.js
@@ -28,7 +28,8 @@ module.exports = {
         flop.eval(game_state, bet);
         break;
       default:
-        bet(game_state.players[game_state.in_action].stack);
+        flop.eval(game_state, bet);
+        // bet(game_state.players[game_state.in_action].stack);
         break;
     }
   },
