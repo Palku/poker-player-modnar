78aec979a36419dabeb1209bec6ef1d190f5f2c1
xzidez <p_macke@bredband.neet>
2015-10-28 15:01:59 +0100

    ace high

--- modules/preflop.js
+++ modules/preflop.js
@@ -4,16 +4,31 @@ var isPremiumHand = function(hole_cards){
   return true;
 };
 
+var acehigh = function(hole_cards){
+  if(hole_cards[0].rank == "A"){
+    return true;
+  }
+  if(hole_cards[1].rank == "A"){
+    return true;
+  }
+  return false;
+}
+
 module.exports = {
 
   eval: function(game_state,bet) {
     var player = game_state.players[game_state.in_action];
     var hole_cards = player.hole_cards;
-    if(isPremiumHand(hole_cards))
-    {
-      bet(500);
+    if(acehigh(hole_cards)){
+      bet(10000000);
+      return;
+    }
+    if(isPremiumHand(hole_cards)){
+      bet(game_state.minimum_raise);
+      return;
     }
   },
 
   isPremiumHand: isPremiumHand,
+  isAceHigh: acehigh
 }
--- test2.js
+++ test2.js
@@ -1,3 +1,14 @@
 var preflopModule = require('./modules/preflop');
 
-preflopModule.test();
+var hole_cards = [                         // The cards of the player. This is only visible for your own player
+                {
+                    "rank": "6",                    // Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+                    "suit": "hearts"                // Suit of the card. Possible values are: clubs,spades,hearts,diamonds
+                },
+                {
+                    "rank": "A",
+                    "suit": "spades"
+                }
+            ];
+console.log(hole_cards);
+console.log("ace high:" + preflopModule.isAceHigh(hole_cards));
