f81d777019269187aa410ebb977ad9014e524750
pulka <a@b.c>
2015-10-28 12:38:46 +0100

    added preflop

--- player.js
+++ player.js
@@ -1,10 +1,13 @@
 
+var preflopModule = require('./modules/preflop');
 module.exports = {
 
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-
+    if(game_state.community_cards.length === 0){
+      preflopModule(eval);
+    }
     bet(game_state.players[game_state.in_action].stack);
     console.log(game_state);
   },
