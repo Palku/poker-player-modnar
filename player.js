7e9044c3c753a76717a02ceb6c28dc1a88a77a4e
xzidez <p_macke@bredband.neet>
2015-10-28 13:11:45 +0100

    add premium hand

--- player.js
+++ player.js
@@ -3,9 +3,21 @@ module.exports = {
 
   VERSION: "Default JavaScript folding player",
 
+  var isPremiumHand = function(hole_cards){
+    return true;
+  };
+
   bet_request: function(game_state, bet) {
-    bet(game_state.players[game_state.in_action].stack);
+    var player = game_state.players[game_state.in_action];
+    var hole_cards = player.hole_cards;
+    if( isPremiumHand(hole_cards)){
+      bet(game_state.players[game_state.in_action].stack);
+    }
+    else {
+      bet(0);
+    }
     return;
+
     if (game_state.community_cards.length === 0) {
       preflop.eval(game_state, bet);
     } else {
