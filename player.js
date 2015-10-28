07b4a55d7beff2bd592e3b0a7ee23b159ba53ef5
pulka <a@b.c>
2015-10-28 13:47:37 +0100

    added flop

--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 //var preflopModule = require('./modules/preflop');
 var preflop = require('./modules/preflop');
+var flop = require('./modules/flop');
 var isPremiumHand = function(hole_cards) {
   return true;
 };
@@ -22,8 +23,11 @@ module.exports = {
       case 0:
         preflop.eval(game_state, bet);
         break;
-        default:
-          bet(game_state.players[game_state.in_action].stack);
+      case 3:
+        flop.eval(game_state, bet);
+        break;
+      default:
+        bet(game_state.players[game_state.in_action].stack);
         break;
     }
   },
